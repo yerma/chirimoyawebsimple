21b391b4b2c90b0d0114d4e41275de7ae4507675
yerma <yerma.araya@gmail.com>
2017-04-29 16:15:38 -0300

    reemplazando ejs por handlebars

--- app/routes.js
+++ app/routes.js
@@ -6,12 +6,47 @@ module.exports = router;
 
 //route to homepage
 router.get('/', function (req, res) {
-    res.render('pages/index');
+    res.render('pages/inicio');
+});
+
+router.get('/nosotros', function (req, res) {
+    res.render('pages/nosotros');
+});
+
+router.get('/servicios', function (req, res) {
+    res.render('pages/servicios');
+});
+
+router.get('/portafolio', function (req, res) {
+    res.render('pages/portafolio');
 });
 
 //route to another page
 router.get('/contacto', function (req, res) {
-    res.send('This is the contact page');
+
+/*    var helper = require('sendgrid').mail;
+
+    from_email = new helper.Email("test@example.com");
+    to_email = new helper.Email("yerma.araya@gmail.com");
+    subject = "Sending with SendGrid is Fun";
+    content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js");
+    mail = new helper.Mail(from_email, subject, to_email, content);
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON()
+    });
+
+    sg.API(request, function(error, response) {
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+    });*/
+
+    res.render('pages/contacto');
+
 });
 
-router.post('/contacto');
+//router.post('/contacto');
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 var express = require('express');
-var expressLayouts = require('express-ejs-layouts');
+var exphbs = require('express-handlebars');
 var app = express();
-var port = process.env.PORT || 8080;
-
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
+var port = process.env.PORT || 3000;
+var path = require('path');
 
 //route the app
 var router = require('./app/routes');
-app.use('/',router);
 
 //static files
 app.use(express.static(__dirname + '/public'));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views',
+    partialsDir: __dirname + '/views/partials'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use('/',router);
 
 //start server
 app.listen(port, function () {
